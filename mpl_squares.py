f8ec52391d55fe47eb94839431a8887be460b225
Rauan <138851982+rauan06@users.noreply.github.com>
2024-02-26 03:02:42 +0600

    Created first project with matplotlib, plotted squares

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 
-squares = [i*i for i in range(1, 10000)]
-plt.plot(squares)
+squares = [i*i for i in range(1, 6)]
+input_values = [i for i in range(1, 6)]
+plt.plot(input_values, squares, linewidth=5)
+
+# Set chart title and label axes.
+plt.title("Function of squres", fontsize=24)
+plt.xlabel("Value", fontsize=12)
+plt.ylabel("Square of Value", fontsize=10)
+
+# Set tick size of labels.
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
