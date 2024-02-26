f6235476da9b1a17d7b4a586bfab7f5792188a75
Rauan <138851982+rauan06@users.noreply.github.com>
2024-02-26 09:23:49 +0600

    Small changes

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 
-input_values = list(range(1,1001))
+input_values = list(range(1,100))
 squares = [i**2 for i in input_values]
 
-plt.scatter(input_values, squares, edgecolors='none', c='red', s=1)
+plt.scatter(input_values, squares, edgecolors='none', c=squares, cmap=plt.cm.Blues, s=10)
 plt.title('Dot of Einstein', fontsize = 10)
 plt.xlabel('See here', fontsize = 10)
 plt.ylabel('And here', fontsize = 10)
 
 
-plt.axis([0, 500, 0, 100000])
+plt.axis([0, 100, 0, 10000])
 plt.tick_params('both', which="major", labelsize=10)
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
