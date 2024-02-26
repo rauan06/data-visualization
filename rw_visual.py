6f9dc1355ea8a18d2cdc5a8c4d486b85fd1332fe
Rauan <138851982+rauan06@users.noreply.github.com>
2024-02-27 01:32:32 +0600

    Small updates

--- rw_visual.py
+++ rw_visual.py
@@ -4,12 +4,12 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, edgecolors='none', 
-                c=point_numbers, cmap=plt.cm.Greys, s=10)
+                c=point_numbers, cmap=plt.cm.Greys, s=1)
     
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
@@ -17,9 +17,13 @@ while True:
                 s=100, c='yellow')
     plt.scatter(rw.x_values[-1], rw.y_values[-1], edgecolors='none',
                 s=100, c='red')
-    
+
     # Remove axes
     plt.axis('off')
+
+    # Set the size of the plotting window
+    # plt.figure(figsize=(10, 6))
+
     plt.savefig('random.png', bbox_inches='tight')
     plt.show()
 
